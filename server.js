89ec0107299e9c48b40e6646e50b66ff44001cf2
nvdinh185 <nvdinh0766777123@gmail.com>
2023-06-08 15:33:32 +0700

    x

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const route = require('./routes');
 // Router init
 route(app);
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/client/index.html');
-});
-
 // start server
 const port = 3000;
 app.listen(port, () => console.log(`Server is starting on port ${port}...`));
